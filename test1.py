3124e048306a1f1e765266e6eb8441513f8636a8
tlgz <tlgamer.gaming@gmail.com>
2025-08-31 18:20:16 +0300

    part2

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ inputs = [1, 2, 3]
 weights = [0.2, 0.8, -0.5]
 bias=2
 
-
+#esitetään yhtä neuronia, joka saa kolme syötettä.
 output = inputs[0]*weights[0]+inputs[1]*weights[1]+inputs[2]*weights[2]+bias
 
 print(output)
